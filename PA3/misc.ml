524ff924d36c380da823abae7ceb1ec683dafacf
Jesse Ren <cs130way@ieng6-201.ucsd.edu>
2016-02-02 22:51:45 -0800

    PA3 Finished and commented

--- PA3/misc.ml
+++ PA3/misc.ml
@@ -1,5 +1,8 @@
 (* CSE 130: Programming Assignment 3
  * misc.ml
+ * Author: Jesse Ren 
+ * Late Days Used: 1 
+ * Date: 2/2/16
  *)
 
 (* For this assignment, you may use the following library functions:
@@ -44,13 +47,23 @@ let sqsum xs =
 
 
 
-(* Basically, takes a list of functions, and arranges them such 
-   that each preceding function is an argument for the next function.
-   ex. fn(...(f2(f1 x))).
 
-   Note: pipe fs returns a FUNCTION, which you use on a SECOND parameter.
-   Hence, the call is ((pipe [function list])) (int)
-*)
+
+
+
+
+
+(*
+ * Name: pipe
+ * Type: (a -> b) List -> (a -> b) 
+ * Function: This function takes in a list of functions, and returns a single function 
+ * that calls each subsequent function on the result of the previous function.  Calling 
+ * the function resulting from pipe will result in all functions being run in a russian doll
+ * like fashion for a single input.
+ * Implementation: Utilizes fold_left, with base as a function that simply returns an input, and the 
+ *   helper function being a function that takes in a function that takes in an input, and returns f2(f1(x))
+ *)
+
 
 (* my mistake was let f a x = (x a) in, it should be a function that
    returns a function!!! but --> that just solves the function.
@@ -63,11 +76,15 @@ let pipe fs =
   let base = (fun x -> x) in
     List.fold_left f base fs
 
-(* Takes in a separator string, and strings together 
-   strings in a list, with the custom separator delimiting each string
+(*
+ * Name: sepConcat
+ * Type: String -> String list -> String
+ * Function: This function takes in a delimiting string for separating, and a list of strings, 
+ * and returns a string of all the strings concatenated with the separator between each original 
+ * element.
+ * Implementation: Simple concatenation using fold_left and accumulating a new element and a separator.
+ * The very first element of the String list, however, does not have a separator.
  *)
-
-(* Sep Concat works like this too...wtf *)
 let sepC sep l = 
   let f a x = a ^ sep ^ x in
   let base = "" in 
@@ -80,6 +97,31 @@ why below option exists *)
          ('a -> 'b -> 'c -> 'd) -> 'e -> 'a -> 'b -> 'c -> 'd
        Type string is not compatible with type 'a -> 'b -> 'c
 *)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let rec sepConcat sep sl = match sl with 
   | [] -> ""
   | h :: t -> 
@@ -102,11 +144,34 @@ let stringOfList f l = "["^ (sepConcat "; " (List.map f l)) ^ "]"
 (******************* 2. Big Numbers ******************************)
 (*****************************************************************)
 
+
+
+(*
+ * Name: clone
+ * Type: int -> a' list 
+ * Function: clones an element n times, depending on the 1st parameter.  Zero for n, or an empty element, 
+ * gives an empty list.
+ * Implementation: Recursively feed back the same parameters, except first one, "n," is returned as "n-1"
+ * in each itereation
+ *)
+
+
+
+
 let rec clone x n = 
     let acc = [] in 
     if (n <= 0) then acc
     else 
         x :: clone x (n-1)
+
+(*
+ * Name: padZero
+ * Type: int List -> int List -> (int List , int List)
+ * Function: Takes two integer lists, and returns a tuple with the two lists, except
+ * the lists will be padded with 0's in front to have the same length
+ * Implementation:
+ *)
+
             
 let rec padZero l1 l2 = 
     let len1 = if l1 = [] then 0 else List.length l1 in 
@@ -119,34 +184,13 @@ let rec padZero l1 l2 =
 		    else 
 			let newl1 = List.append [0] l1 in
 			padZero newl1 l2
-(* For Testing *)
 (*
-open Printf
-let testList =  (List.append [0] [2;3])
-(*let _= List.iter (print_int) testList;; *)
-
-let tupleCombine (x,y) = 
-	List.append x y;;
-
-
-let _= List.iter (print_int) (tupleCombine (padZero [0][2;3]));;
-let _= print_string "\n";;
-let _= List.iter (print_int) (tupleCombine (padZero [2;3][1]));;
-let _= print_string "\n";;
-let _= List.iter (print_int) (tupleCombine (padZero [][2;3]));;
-let _= print_string "\n";;
-let _= List.iter (print_int) (tupleCombine (padZero [2;3][]));;
-let _= print_string "\n";;
-let _= print_int (List.length []);;
-let _= print_string "\n";;
-let _= print_int (List.length [3;2]);;
-let _= print_string "\n";;
-
-padZero [9;9] [1;0;0;2];;
-(*- : int list * int list = ([0;0;9;9],[1;0;0;2]) *)
-padZero [1;0;0;2] [9;9];;
-(*- : int list * int list = ([1;0;0;2],[0;0;9;9]) *)
-*)
+ * Name: removeZero 
+ * Type: int List -> int List 
+ * Function: Strips away the zeroes in front of a list of ints.
+ * Implementation: Recursively check if head is a 0, and if so, return the tail to itself, until the
+ * head of the list is non-zero. 
+ *)
 
 
 let rec removeZero l = match l with 
@@ -154,10 +198,24 @@ let rec removeZero l = match l with
     | h::t -> if h == 0 then removeZero t 
                   else h::t 
 
-    (* Note that base should match what args is,because base = base case *)
-    (* Number on left is remainder *)
-    (* Trick is to have a list of tupled ints *,
-     because of the way fold_left works *)
+(* Name: bigAdd
+ * Type: int List -> int List -> int List
+ * Function:  Adds two big integers represented as a list of ints for each digit
+ * Implementation: Adds in each level, the integers, while keeping track of 
+ * a remainder for each
+ * recursion to the next step if needed.
+ *)
+
+
+
+
+
+
+
+
+
+
+
 let bigAdd l1 l2 = 
   let add (l1, l2) = 
     let f a x = 
@@ -183,12 +241,14 @@ let bigAdd l1 l2 =
   in 
     removeZero (add (padZero l1 l2));;
 
+(* Testing 
 let x = [9;9;9;9];;
 let y = [9;9;9];;
 let z = [1;0;9;9;8];;
 
 let _= List.iter (print_int) (bigAdd [1;2][1;2]);;
 let _= List.iter (print_int) (bigAdd x y );; 
+*)
 
 (* fold_left implementation: 
   let rec fold_left f a l = 
@@ -197,16 +257,88 @@ let _= List.iter (print_int) (bigAdd x y );;
 	| h::t -> fold_left f (f a h) t 
 *)
 
+
+
+(* Helper function from Pa1 *)
+let rec digitsOfInt n = match n with
+| 0 -> []
+| x ->
+digitsOfInt (n / 10)@[(n mod 10)]
+
+
+
+
+
+(*
+ * Name: mulByDigit
+ * Type: int -> int List -> int [List]
+ * Function: Multiplies a number (represented by a list, with each element as a digit), by an int
+ * Implementation: Uses digitsofInt from PA1 to change products with more than 1 digit to a list.
+ * This function starts from the head, and pads zeroes based on the length of the remaining list (parsed
+ * by one element.  Ex: in 3 * [3;2;1], it, in first level, will add 
+ * [3*3]::[length-1 # of Zeroes], which is [900], to the accumulator, which is [0] in the beginning
+ *)
+
+
 let rec mulByDigit i l = 
+  match l with 
+  | [] -> [0] 
+  | h::t -> 
+	let prod = h * i in 
+        let padProd = 
+        if ((prod)>9) then digitsOfInt prod else [prod] in 
+        let param1 = 
+	List.append (padProd) (clone 0 (List.length t)) in 
+        let param2 = (mulByDigit i t) in 
+        bigAdd (param1) (param2)
+
+let x = [9;9;9;9];;
+let y = 9;;
+(*
+let _= print_string "\nJesse\n";;
+let _= List.iter print_int (mulByDigit 9 [9]);;
+let _= print_string "\nRen\n";;
+let _= List.iter print_int (mulByDigit 9 x);;
+let _= print_string "\nRocks\n";;
+let _= List.iter print_int (mulByDigit 1 x);;
+*)
+
+
+(*
   let f a h = (i * h)::a in 
   match l with 
-    | [] -> a 
-    | h::t -> mulByDigit (f a h) t 
+    | [] -> []
+    | [ht,ht'] -> ht'
+    | h::t -> (mulByDigit i t) ::
+*)
 
+
+
+(*
+ * Name: bigMul
+ * Type: int List -> int List -> int List 
+ * Function: Multiplies two large numbers, which are represented as lists, with each of their 
+ * digits as an element.
+ * Implementation: Uses mulByDigit to multiply each element of l2 to the entirety of l1.  Then, uses
+ * bigAdd to add all the products of [elements of l2 * l1]
+ * Ex: bigMul [2;2;2;2] [3;3] -> will add 3 * 2222 and 3 *2222, with correct 0's added for levels.
+ *)
 
 let bigMul l1 l2 = 
-  let f a x = failwith "to be implemented" in
-  let base = failwith "to be implemented" in
-  let args = failwith "to be implemented" in
+  let f a x =  
+    let (lvl, acc) = a in 
+    let product = mulByDigit x l1 in 
+    let product2 = List.append product (clone 0 lvl) in 
+    let fproduct = bigAdd acc product2 in 
+    (lvl+1, fproduct) in 
+  let base = (0,[0]) in 
+  let args = List.rev l2 in
   let (_, res) = List.fold_left f base args in
     res
+
+
+
+let _= print_string "\nRen\n";;
+let _= List.iter print_int (bigMul [3] [9;1]);;
+let _= print_string "\nRen\n";;
+let _= List.iter print_int (bigMul [0] [9;1]);;
